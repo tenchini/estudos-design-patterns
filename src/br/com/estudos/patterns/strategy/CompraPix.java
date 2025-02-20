6603a13fac7aee862bc612e3f35f0dee29660b92
tenchini <tenchini.dev@gmail.com>
2025-02-20 14:39:57 -0300

    Modifica retorno da classe CompraPix

--- src/br/com/estudos/patterns/strategy/CompraPix.java
+++ src/br/com/estudos/patterns/strategy/CompraPix.java
@@ -6,7 +6,7 @@ public class CompraPix implements StrategyCompra {
   @Override
   public String pagar(Item item) {
     double valorComDesconto = item.getPreco() * 0.9;
-    System.out.println("Compra realizada no crédito: " + item + ", valor com desconto de 10% do PIX: R$" +String.format("%.2f", valorComDesconto)+ ", obrigado pela compra!");
+    System.out.println("Compra realizada no PIX: " + item + ", valor com desconto de 10% do PIX: R$" +String.format("%.2f", valorComDesconto)+ ", obrigado pela compra!");
     return "";
   }
   }
